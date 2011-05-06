222fff5c5931ef2a7f2a2038289904e913d3a1c8
Caio Lima <caio.p.a.lima@gmail.com>
2011-05-05 22:35:32 -0400

    first commit

---
+++ MadLibz/InsertWords.h
@@ -0,0 +1,16 @@
+//
+//  InsertWords.h
+//  MadLibz
+//
+//  Created by Caio Lima on 11-05-05.
+//  Copyright 2011 Communitech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface InsertWords : UITableViewController {
+    
+}
+
+@end
